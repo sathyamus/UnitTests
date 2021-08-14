2ca4322fc0855037dcb43684ddd2c29bc8d1651c
sathyamus <sathyamus.sify@gmail.com>
2021-08-14 22:46:17 +0530

    (feat): Refactored

--- src/main/java/com/snsystems/SortService.java
+++ src/main/java/com/snsystems/SortService.java
@@ -9,16 +9,20 @@ import java.util.List;
  *
  */
 public class SortService {
+	
+	final String SPLIT_PATTERN = ",";
 
 	public String sortingByIgnoreCase(String input) {
-		List<String> strings = Arrays.asList(input.split(","));
+		List<String> strings = Arrays.asList(input.split(SPLIT_PATTERN));
 		strings.sort(String::compareToIgnoreCase);
 		return strings.toString();
 	}
-	
+
 	public String sortingByNatural(String input) {
-		List<String> strings = Arrays.asList(input.split(","));
+		List<String> strings = Arrays.asList(input.split(SPLIT_PATTERN));
 		Collections.sort(strings);
 		return strings.toString();
-	}	
+	}
+	
+	
 }

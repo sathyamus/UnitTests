7efa9971680921ed5ff8eb5ae39f5b173d4cd701
sathyamus <sathyamus.sify@gmail.com>
2021-09-20 21:19:28 +0530

    (feat): Lambda predicate negate, updated with expected results

--- src/test/java/com/snsystems/LambdaTest.java
+++ src/test/java/com/snsystems/LambdaTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -38,11 +37,15 @@ public class LambdaTest {
 		System.out.println("removeIf :");
 		colors2.removeIf(endingWithE);
 		colors2.forEach(color -> System.out.println("- " + color));
+		String[] expectedResults1 = {"red", "green", "yellow"}; 
+		assertThat(colors1).contains(expectedResults1);
 		
 		System.out.println("removeIf v2:");
 		colors2 = new ArrayList<>(Arrays.asList(colors));
 		colors2.removeIf(color -> color.endsWith("e"));
 		colors2.forEach(color -> System.out.println("- " + color));
+		String[] expectedResults2 = {"red", "green", "yellow"}; 
+		assertThat(colors1).contains(expectedResults2);
 		
 		assertThat(colors2).hasSize(3);
 		
@@ -54,6 +57,8 @@ public class LambdaTest {
 		// For making re-use of stream, re-creating them
 		filtered = colors2.stream().filter(endingWithE);
 		assertThat(filtered).hasSize(3);
+		String[] expectedResults3 = {"red", "green", "yellow"}; 
+		assertThat(colors1).contains(expectedResults3);
 	}
 	
 	@Test
@@ -66,7 +71,6 @@ public class LambdaTest {
 			return "Hello";
 		};
 		assertThat(supplier.get()).isEqualTo("Hello");
-		
 	}
 	
 	@Test
@@ -79,7 +83,6 @@ public class LambdaTest {
 		};
 		consumer.accept("Hello");
 		consumer.accept("Hello World!");
-		
 	}
 	
 	@Test
@@ -94,16 +97,31 @@ public class LambdaTest {
 		System.out.println("colors After filter :");
 		colors1.removeIf(filterByE);
 		assertThat(colors1).hasSize(3);
+		String[] expectedResults = {"red", "green", "yellow"}; 
+		assertThat(colors1).contains(expectedResults);
+
+	}
+	
+	
+	@Test
+	public void negativeFilterStringsV2() {
 		
+		String[] colors = {"red", "green", "blue", "orange", "purple", "yellow"	};
+		List<String> colors1 = new ArrayList<>(Arrays.asList(colors));
+		System.out.println("colors Before remove :");
+		assertThat(colors1).hasSize(6);
+		Predicate<String> filterByE = (String input) -> input.endsWith("e");
 		
-		
+		System.out.println("colors After filter :");
+		colors1.removeIf(filterByE.negate());
+		assertThat(colors1).hasSize(3);
+		String[] expectedResults = {"blue", "orange", "purple"}; 
+		assertThat(colors1).contains(expectedResults);
 	}
 	
 	@Test
 	public void function() {
-		Function<String[], Integer> length = (String[] input) -> {
-			return input.length;
-		};
+		Function<String[], Integer> length = (String[] input) -> input.length;
 		
 		String[] colors = {"red", "green", "blue", "orange", "purple", "yellow"	};
 		assertThat(length.apply(colors)).isEqualTo(6);

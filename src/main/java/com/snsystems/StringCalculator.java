f31dc1414927eb31763e627bef0c65c3b9074863
sathyamus <sathyamus.sify@gmail.com>
2021-09-20 23:16:28 +0530

    (feat): TDD Kata - emptyStringReturnZero - post refactoring

--- src/main/java/com/snsystems/StringCalculator.java
+++ src/main/java/com/snsystems/StringCalculator.java
@@ -4,10 +4,14 @@ public class StringCalculator {
 	
 
 
-	public int calculate(String string) {
-		if (string.isEmpty()) {
+	public int calculate(String input) {
+		if (checkIfStringEmpty(input)) {
 			return 0;
 		}
 		return -1;
+	}
+
+	private boolean checkIfStringEmpty(String input) {
+		return input.isEmpty();
 	}
 }

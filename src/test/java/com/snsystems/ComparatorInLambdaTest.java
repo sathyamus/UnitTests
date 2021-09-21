8d92b68af7f94f8b2f3b302b37b3af07b6d06109
sathyamus <sathyamus.sify@gmail.com>
2021-09-21 23:46:21 +0530

    (feat): Comparator using chain of lambdas

--- src/test/java/com/snsystems/ComparatorInLambdaTest.java
+++ src/test/java/com/snsystems/ComparatorInLambdaTest.java
@@ -66,6 +66,47 @@ public class ComparatorInLambdaTest {
 		digits.sort(lambdaComparator);
 		assertThat(digits).isEqualTo(Arrays.asList("one", "two", "six", "four", "five", "three"));
 		
+	}
+	
+	@Test
+	public void compareUsersByNameAndAge() {
+		
+		User sathya = new User("Sathya", 35);
+		User lr = new User("LR", 35);
+		User dsp = new User("DSP", 10);
+		User ssp1 = new User("SSP", 5);
+		User ssp2 = new User("SSP", 3);
+		
+		List<User> users = Arrays.asList(sathya, lr, dsp, ssp1, ssp2);
+		
+		users.forEach(user -> System.out.println(user));
+		users.forEach(user -> System.out.println("Name : " + user.name + ", Age : " + user.age));
+		
+		// chain of lambdas
+		Comparator<User> comparatorByName = Comparator.comparing(user -> user.name);
+		Comparator<User> comparatorByAge = Comparator.comparingInt(user -> user.age);
+		
+		users.sort(comparatorByName.thenComparing(comparatorByAge));
+		users.forEach(user -> System.out.println(user));
+		assertThat(users).containsSequence(dsp, lr, ssp2, ssp1, sathya);
+		
+	}	
+	
+	private class User {
+		
+		private String name;
+		private Integer age;
+		
+		public User(String name, Integer age) {
+			super();
+			this.name = name;
+			this.age = age;
+		}
+		
+		@Override
+		public String toString() {
+			return "[ User: " + name + ", Age: " + age + "]";
+		}
 		
 	}
 }

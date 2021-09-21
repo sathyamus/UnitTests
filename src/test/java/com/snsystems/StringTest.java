179fdeef15907f8827261db874490dd02e3ec4fc
sathyamus <sathyamus.sify@gmail.com>
2021-09-21 22:05:57 +0530

    (bug): Fixed compilation

--- src/test/java/com/snsystems/StringTest.java
+++ src/test/java/com/snsystems/StringTest.java
@@ -1,31 +1,31 @@
 package com.snsystems;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class StringTest {
 	
-	StringCalculator calc = new StringCalculator();
-	
-	
-	@Test
-	public void should_return_sum_after_split() {
-		
-		assertThat(calc.sum("3,4")).isEqualTo(7);
-		assertThat(calc.sum("1,2,3")).isEqualTo(6);
-		assertThat(calc.sum("1,2,3,4")).isEqualTo(10);
-	}
-	
-	@Test
-	public void should_return_sum_after_split_by_carriage() {
-		assertThat(calc.sum("1,2\n3")).isEqualTo(6);
-	}
-
-
 	@Test
 	public void concatination() {
-		assertThat(2 + 3 + "Sathya" + 2 + 3).isEqualTo("5Sathya23");
+		String result = 2 + 3 + "Sathya" + 2 + 3;
+		assertThat(result).isEqualTo("5Sathya23");
+		
+		String result2 =  2 + 3 + "Sathya";
+		result2 += "Sathya" + 2 * 3;
+		assertThat(result2).isEqualTo("5SathyaSathya6");
+		
+		
+		String s1 = "Hello";
+		String s2 = "Hello";
+		String s3 = " Hello";
+		assertTrue(s1 == s2);
+		assertThat(s3.substring(1)).isEqualTo("Hello");
+		assertThat(s3.substring(1)).isEqualTo("Hello");
+		assertFalse(s1 == s3.substring(1));
+		assertTrue(s1.equals(s3.substring(1)));
 	}
 
 }
